6a66fd1ead88ef95f2d726573d70ccbdf9a44dba
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-18 05:13:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-    
+  if phrase == phrase.uppercase
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
